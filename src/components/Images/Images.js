4ae2f9f4f020289588d8acb1b63e1b50b0f4b0f3
Renszo Camacho <varsovia84@gmail.com>
2021-01-11 02:37:35 +0100

    Fixing favicon issue.

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -3,44 +3,43 @@ import React from "react"
 import Img from "gatsby-image"
 import { graphql, useStaticQuery } from "gatsby"
 
-function images() {
+function Images() {
   const data = useStaticQuery(graphql`
-  query {
-    allFile (
-      filter {
-        extension: {regex: "/(jpg)|(jpeg)|(png)/"}
-        name: {nin: ["background", "background2"]}
-      }
-    }
-  ){
-    edges {
-      node {
-        base 
-        childImageSharp {
-          fluid {
-            ....GatsbyImageSharpFluid
+    query {
+      allFile(
+        filter: {
+          extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+          name: { nin: ["background", "background2"] }
+        }
+      ) {
+        edges {
+          node {
+            base
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
-  }
   `)
   return (
     <div>
       <h1>View our Destination</h1>
       <div>
-        {data.allFile.edges.node.map((image, key) => {
-          return (
-            <Img
-              key={key}
-              fluid={image.childImageSharp.fluid}
-              alt={image.base.split(".")[0]}
-            />
-          )
-        })}
+        {data.allFile.edges.map((image, key) => (
+          <Img
+            key={key}
+            className="imagen-item"
+            fluid={image.node.childImageSharp.fluid}
+            alt={image.node.base.split(".")[0]}
+          />
+        ))}
       </div>
     </div>
   )
 }
 
-export default images
+export default Images
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/Hero/Hero"
+import Image from "../components/Images/Images"
 
 const IndexPage = () => (
   <Layout>
